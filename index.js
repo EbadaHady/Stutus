f24df3876813c2dc9b1937ce59ef6be8328736dd
EbadaHady <159580342+EbadaHady@users.noreply.github.com>
2024-02-09 22:21:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ client.on('ready', async () => {
     .setName('EBADA Community')
     .setDetails(`الـخـطـاف`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1201191374893105263/1205601973613760562/200_2.gif') //You can put links in tenor or discord and etc.
+ .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1201191374893105263/1205608988276949013/200-2.gif') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('الـخـطـاف') //Text when you hover the Large image
     .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1077716803435909232/1169543166999937024/Verification.png') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('Social Media Management') //Text when you hover the Small image
